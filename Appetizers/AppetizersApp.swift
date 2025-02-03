75565c7c0d205dade914419df85afefcccbbe19a
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-03 23:08:11 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ömer Faruk Dikili on 3.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizersTests/AppetizersTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersTests.swift
+//  AppetizersTests
+//
+//  Created by Ömer Faruk Dikili on 3.02.2025.
+//
+
+import Testing
+@testable import Appetizers
+
+struct AppetizersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
